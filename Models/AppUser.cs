6ae048910c9b6b420a807b3648f6c03037f1d75d
Yaroslav Shevchenko <schev4enko3@gmail.com>
2021-11-12 01:26:11 +0300

    Expand IdentityUser model

---
+++ Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebRecommend.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FullName { get; set; }
+        public int Reputation { get; set; }
+    }
+}
